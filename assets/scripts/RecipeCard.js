c3bf72b2fb3e8c4b714ee8592316859673f8b87d
hoz011 <114896112+hollyzhu573@users.noreply.github.com>
2023-05-14 23:21:21 -0700

    Edit main.js and RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -14,7 +14,7 @@ class RecipeCard extends HTMLElement {
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
     let styleEl = document.createElement('style');
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
-    styleEl.textContent = `
+    styleEl.innerHTML = `
       * {
         font-family: sans-serif;
         margin: 0;
@@ -92,8 +92,8 @@ class RecipeCard extends HTMLElement {
       }
     `;
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-    shadow.append(styleEl);
-    shadow.append(articleEl);
+    shadow.appendChild(styleEl);
+    shadow.appendChild(articleEl);
   }
 
   /**
@@ -122,26 +122,27 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-    let article_select = shadow.querySelector('article');
+    let art = document.getElementByID('article');
+    art.attachShadow({mode: "open"});
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
-    //           literals (tempalte strings) and element.innerHTML for this.
-    article_select.innerHTML = `
-      <img src="https://link-to-article.com/recipe-thumbnail.jpg"
-        alt="Recipe Title">
+    //           literals (template strings) and element.innerHTML for this.
+    art.shadowRoot.innerHTML = `
+      <img src="${data.imgSrc}"
+        alt="${data.imgAlt}">
       <p class="title">
-        <a href="https://link-to-article.com">Title</a>
+        <a href="${data.titleLnk}">${data.titleTxt}</a>
       </p>
-      <p class="organization">The Chef's Organization</p>
+      <p class="organization">${data.organization}</p>
       <div class="rating">
-        <span>5</span>
-        <img src="/assets/images/icons/5-star.svg" alt="5 stars">
-        <span>(500)</span>
+        <span>${data.rating}</span>
+        <img src="./assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+        <span>(${data.numRatings})</span>
       </div>
-      <time>50 min</time>
+      <time>${data.lengthTime}</time>
       <p class="ingredients">
-        Comma, Separated, List, of, Ingredients
+        ${data.ingredients}
       </p>
     `;
   }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,7 +24,7 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
-  const recipesString = localStorage.getItem('recipes');
+  const recipesString = window.localStorage.getItem('recipes');
   if (recipesString) {
     return JSON.parse(recipesString);
   } else {
@@ -41,17 +41,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
-  const mainEl = document.querySelector('main');
+  let mainEl = document.querySelector('main');
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
   recipes.forEach(r => {
-    const recipeCard = document.createElement('recipe-card');
-    recipeCard.title = r.title;
-    recipeCard.ingredients = r.ingredients;
-    recipeCard.instructions = r.instructions;
-    mainEl.appendChild(recipeCard);
+    let recipeCard = document.createElement('recipe-card');
+    recipeCard.data = r.data;
+
+    mainEl.append(recipeCard);
   });
 }
 
