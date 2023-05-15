d405e4c38892b67006253f31c2633b620e49a9bd
hoz011 <114896112+hollyzhu573@users.noreply.github.com>
2023-05-14 19:20:20 -0700

    Select article and add template

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -9,13 +9,12 @@ class RecipeCard extends HTMLElement {
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
     let shadow = this.attachShadow({mode: 'open'});
-    let rootEl = document.createElement('div');
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
     let articleEl = document.createElement('article');
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
     let styleEl = document.createElement('style');
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
-    styleEl.innerText = `
+    styleEl.textContent = `
       * {
         font-family: sans-serif;
         margin: 0;
@@ -93,8 +92,8 @@ class RecipeCard extends HTMLElement {
       }
     `;
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-    rootEl.append(styleEl);
-    rootEl.append(articleEl);
+    shadow.append(styleEl);
+    shadow.append(articleEl);
   }
 
   /**
@@ -123,10 +122,28 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+    let article_select = document.getElementsByTagName('article');
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
+    article_select.innerHTML = `
+      <img src="https://link-to-article.com/recipe-thumbnail.jpg"
+        alt="Recipe Title">
+      <p class="title">
+        <a href="https://link-to-article.com">Title</a>
+      </p>
+      <p class="organization">The Chef's Organization</p>
+      <div class="rating">
+        <span>5</span>
+        <img src="/assets/images/icons/5-star.svg" alt="5 stars">
+        <span>(500)</span>
+      </div>
+      <time>50 min</time>
+      <p class="ingredients">
+        Comma, Separated, List, of, Ingredients
+      </p>
+    `;
   }
 }
 
